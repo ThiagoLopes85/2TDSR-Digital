08c7b5eed30a3d10d0a89fe7ceab0374c06bab71
Logon PF Fiap <logonpflocal@fiap.com.br>
2018-08-09 22:06:57 -0300

    Spring MVC Exercício

--- 02-Spring-MVC/src/br/com/fiap/spring/controller/AcademiaController.java
+++ 02-Spring-MVC/src/br/com/fiap/spring/controller/AcademiaController.java
@@ -6,25 +6,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.fiap.spring.model.Carro;
+import br.com.fiap.spring.model.Aluno;
 
 @Controller
-@RequestMapping("carro")
-public class CarroController {
+@RequestMapping("academia")
+public class AcademiaController {
 
 	@GetMapping("cadastrar")
 	public String abrirForm() {
-		return "carro/form";
+		return "academia/form"; //pasta + JSP
 	}
 	
 	@PostMapping("cadastrar")
-	public ModelAndView processarForm(Carro c) {
-		
-		System.out.println(c.getPlaca() + " " + c.getPreco() + " " 
-				+ c.isAutomatico() + " " + c.getMarca());
-		
-		ModelAndView retorno = new ModelAndView("carro/form");
-		retorno.addObject("carro", c);
+	public ModelAndView processarForm(Aluno aluno) {
+		ModelAndView retorno = 
+				new ModelAndView("academia/sucesso");
+		retorno.addObject("msg", "Aluno registrado");
+		retorno.addObject("a", aluno);
 		return retorno;
 	}
+	
 }
